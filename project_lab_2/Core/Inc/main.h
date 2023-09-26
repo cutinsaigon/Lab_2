7589ea4a360bb2a534656a96e4297bf75bcbb6a4
cutinsaigon <lenhattien2882@gmail.com>
2023-09-27 00:19:19 +0700

    Setup demo

--- project_lab_2/Core/Inc/main.h
+++ project_lab_2/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_Pin GPIO_PIN_5
+#define LED_RED_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
